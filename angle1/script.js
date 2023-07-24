4f168419f06ecec928da64b53cdb82fa22f133ae
PrasannaP <mail2lulu@gmail.com>
2023-07-24 23:53:17 +0530

    TODO: updatePointPos

--- angle1/script.js
+++ angle1/script.js
@@ -30,40 +30,92 @@
         return l
     }
 
+
+
+    function updatePointPos(p) {
+
+    }
+    function updatePoint(p) {
+
+        deltaLeft = p.left - centerPointX;
+        deltaTop = p.top - centerPointY;
+
+        var length = Math.sqrt(deltaLeft * deltaLeft + deltaTop * deltaTop);
+        // if (length <= centerRadius - 5 || length >= centerRadius + 5) {
+        var radians = Math.atan2(deltaTop, deltaLeft)
+        /**
+         * Find the angle from radians
+         */
+        var ang = radians * 180 / Math.PI;
+        // console.log("pkp:  ~ file: script.js:62 ~ ang:", ang)
+
+        length = centerRadius;
+
+        p.left = Math.cos(radians) * length + centerPointX
+        p.top = Math.sin(radians) * length + centerPointY
+        // } // end if length by 5
+
+        if (p.line.num == 1) {
+            // if line 1 then change the angle
+            p.line.set({
+                angle: ang
+            })
+
+            if (p.label == "point1") {
+                console.log("pkp:  ~ file: script.js:62 ~ updatePoint ~ p.label:", p.label)
+
+                console.log("pkp:  ~ file: script.js:66 ~ updatePoint ~  p.line.x2:", p.line.x2)
+                // p2.set({
+                //     left: p.line.x2,
+                //     top: p.line.y2
+                // })
+
+                updatePointPos(p2)
+            } else {
+
+            }
+
+
+        } else {
+            // else change the top of the line to points position
+            p.line && p.line.set({ 'x2': p.left, 'y2': p.top });
+        }
+        p.setCoords()
+        canvas.renderAll();
+    }
+    canvas.on('object:moving', function (e) {
+        var p = e.target;
+
+
+        updatePoint(p, "on update")
+
+    });
+
+
     var line1 = makeLine([100, 300, 500, 300], "red", 1),
         line2 = makeLine([300, 300, 200, 100], "green", 2),
         line3 = makeLine([300, 300, 300, 150], "blue", 3)
 
-    canvas.add(line1, line2, line3);
+    var p1, p2, p3, p4;
 
-    canvas.add(
-        makeCircle(line1.x1, line1.y1, line1, "green", "point1"),
-        makeCircle(line1.x2, line1.y2, line1, "green", "point2"),
-        makeCircle(line2.x2, line2.y2, line2, "red"),
-        makeCircle(line3.x2, line3.y2, line3, "red")
-    );
 
-    canvas.on('object:moving', function (e) {
-        var p = e.target;
-        // p.line && p.line.set({ 'x2': p.left, 'y2': p.top });
-        console.log("pkp:  ~ file: script.js:49 ~ p:", p.line.angle)
+    var centerPointX = 300,
+        centerPointY = 300,
+        centerRadius = 200
 
-        p.line.set({
-            angle: p.line.angle + 10
-        })
-        console.log("pkp:  ~ file: script.js:54 ~ p.line:", p.line)
-        // p.set({
-        //     left: 100,
-        //     top: 100
+    function init() {
+        canvas.add(line1, line2, line3);
+        p1 = makeCircle(line1.x1, line1.y1, line1, "green", "point1")
+        p2 = makeCircle(line1.x2, line1.y2, line1, "green", "point2")
+        p3 = makeCircle(line2.x2, line2.y2, line2, "red")
+        p4 = makeCircle(line3.x2, line3.y2, line3, "red")
+        canvas.add(
+            p1, p2, p3, p4
+        );
 
-        // })
-        // console.log("pkp:  ~ file: script.js:48 ~ p.line:", p.line)
-        if (p.line.num == 1) {
-            // console.log("pkp:  ~ file: script.js:52 ~ p.line.num:", p.line.num)
+        updatePoint(p3)
+        updatePoint(p4)
+    }
+    init()
 
-        } else {
-
-        }
-        canvas.renderAll();
-    });
 })();
